6458bef2d92820e7ceefed02f5e02ecd14fbfa7b
Alestirrondo <alestirrondo@gmail.com>
2024-08-07 17:24:51 -0400

    updating connection methods

--- acronymgameserver/index.js
+++ acronymgameserver/index.js
@@ -4,7 +4,7 @@ app.get("/", (req,res)=> res.sendFile(__dirname + "/index.js"))
 app.listen(9091, () => console.log("Listening on http port 9091"))
 const websocketServer = require("websocket").server;
 const httpServer = http.createServer();
-httpServer.listen(9090, () => console.log("Listening.. on 9090"));
+httpServer.listen("https://acronymforme.onrender.com/");
 
 const clients = {};
 const games = {};
