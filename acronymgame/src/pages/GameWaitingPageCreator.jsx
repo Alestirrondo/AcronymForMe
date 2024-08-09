54e5a080a6c41fa573961e4e5a4a2e167b1ec249
Alestirrondo <alestirrondo@gmail.com>
2024-08-09 04:16:55 -0400

    running too many times

--- acronymgame/src/pages/GameWaitingPageCreator.jsx
+++ acronymgame/src/pages/GameWaitingPageCreator.jsx
@@ -37,7 +37,7 @@ const GameWaitingPageCreator = ({location}) => {
           
         }
         
-      })
+      },[])
     
     useEffect(() => {
         if(location.pathname === '/GameWaitingPageCreator'){
